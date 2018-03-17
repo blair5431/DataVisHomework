8680b18807e9a252de661f33b7f1a0cfe47875fe
Blair Turner <blairturner@MacBook-Pro.local>
2018-03-16 19:18:57 -0500

    social analytics hw

---
+++ Social_Analytics/config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = 'KAWheRzKw2GTSIrDbP6HNh2zC'
+consumer_secret = '7nN9GfiyyQ3PpNKURCEGlfoYESGQFi6CpOyAfC48TM7f7dkw2a'
+access_token = '67515862-tVokYnhsG4QNp4AHvgqGNZwccK5EorPN1ZXHXLBoo'
+access_token_secret = 'TdfbITxHGlHy1X3f5CZ5CVyW3paQMaVpsPVYFUQ863xQR'
