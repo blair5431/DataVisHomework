ee11e72a7f614d3d35682876bee930d09dbec466
Blair Turner <blairturner@dhcp-10-102-133-87.wireless.northwestern.private>
2018-05-08 18:26:15 -0500

    updated js hw

--- java_script/index.js
+++ java_script/index.js
@@ -1,12 +1,12 @@
 // Get references to the tbody element, input field and button
 var $tbody = document.querySelector("tbody");
-var $stateInput = document.querySelector("#datetime");
+var $dateInput = document.querySelector("#datetime");
 var $searchBtn = document.querySelector("#search");
 
 // Add an event listener to the searchButton, call handleSearchButtonClick when clicked
-// $searchBtn.addEventListener("click", handleSearchButtonClick);
+$searchBtn.addEventListener("click", handleSearchButtonClick);
 
-// Set filteredAddresses to addressData initially
+// Set filteredufo to ufo dataset initially
 var filteredufo = dataSet;
 
 // renderTable renders the filteredufo to the tbody
@@ -27,19 +27,22 @@ function renderTable() {
   }
 }
 
-// function handleSearchButtonClick() {
+function handleSearchButtonClick() {
 //   // Format the user's search by removing leading and trailing whitespace, lowercase the string
-//   var filterState = $stateInput.value.trim().toLowerCase();
+  var filterDate = $dateInput.value;
+  console.log(filterDate);
 
-//   // Set filteredAddresses to an array of all addresses whose "state" matches the filter
-//   filteredAddresses = addressData.filter(function(address) {
-//     var addressState = address.state.toLowerCase();
+//   // Set filtereddate to an array of all info whose "date" matches the filter
+  filteredufo = dataSet.filter(function(ufo) {
+    var ufoDate = ufo.datetime;
+    console.log(ufoDate);
+    console.log(filterDate);
 
-//     // If true, add the address to the filteredAddresses, otherwise don't add it to filteredAddresses
-//     return addressState === filterState;
-//   });
-//   renderTable();
-// }
+//     // If true, add the ufo to the filteredDated, otherwise don't add it to filteredDate
+    return ufoDate === filterDate;
+  });
+  renderTable();
+}
 
 // Render the table for the first time on page load
 renderTable();
